5150a3f9df3a208570f54b6e0153a2ef3ffa0537
Joaquín Menchaca <jmenchaca@verticalresponse.com>
2014-10-06 19:56:06 -0700

    java constructor update

--- java/b00.construct/Demo.java
+++ java/b00.construct/Demo.java
@@ -1,14 +1,13 @@
 public class Demo {
-    public static void main(String args[])
-    {      
-        // initialize data through mutator (set)
-        Person person = new Person(); // instantiate new object
-        p.setName("Jean-Luc");
+  public static void main(String args[])
+  {      
+    // initialize data through constructor
+    Person person = new Person("Jean-Luc"); // instantiate new object w/ data
 
-        // retrieve string from person object
-        String name = person.getName(); // accessor
+    // retrieve string from person object
+    String name = person.getName(); // accessor
 
-        // output results
-        System.out.println("Name of the Person:\n\t" + name);
-    }
+    // output results
+    System.out.println("Name of the Person:\n\t" + name);
+  }
 }
