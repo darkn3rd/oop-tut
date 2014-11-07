c89fcb6fa8355d6bc7bf16f43696b6e60f77008d
Joaquín Menchaca <jmenchaca@verticalresponse.com>
2014-11-07 15:04:14 -0800

    java overloaded constructors

---
+++ java/c00.overload/Demo.java
@@ -0,0 +1,34 @@
+public class Demo {
+    public static void main(String args[])
+    {      
+        String name;
+        Integer age;
+
+        // initialize data through different constructors (overloading)
+        Person captain = new Person("Jean-Luc");   // instantiate w name
+        Person officer = new Person(21);           // instantiate w age
+        Person ensign  = new Person("Wesley", 15); // instantiate w name and age
+
+        // retrieve string from person object
+        name = captain.getName(); // accessor
+        age  = captain.getAge();  // accessor
+
+        // output results
+        System.out.println("Captain:\n\tName: " + name + "\n\tAge:  " + age);
+
+        // retrieve string from person object
+        name = officer.getName(); // accessor
+        age =  officer.getAge();  // accessor
+
+        // output results
+        System.out.println("Officer:\n\tName: " + name + "\n\tAge:  " + age);
+
+        // retrieve string from person object
+        name = ensign.getName(); // accessor
+        age  = ensign.getAge();  // accessor
+
+        // output results
+        System.out.println("Ensign:\n\tName: " + name + "\n\tAge:  " + age);
+
+    }
+}
