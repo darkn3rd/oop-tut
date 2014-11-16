e5852d32af778721a63fbad9279074cf89fa5f28
지훈 <sauvecali@yahoo.com>
2014-11-16 14:03:41 -0800

    Java: Overloaded Constructor Chaining

---
+++ java/c01.chaining/Person.java
@@ -0,0 +1,24 @@
+public class Person
+{
+    private String name = "";
+    private Integer age = 0;
+
+    // Constructor
+    public Person(String name)  { this(name, -1); }
+    public Person(Integer age)  { this("NULL", age); }
+
+    public Person(String name, Integer age)
+    {
+        this.setName(name);
+        this.setAge(age);
+    }
+
+    // Mutators - functions that set stuff
+    public void   setName(String name) { this.name = name; }
+    public void   setAge(Integer age)  { this.age = age; }
+
+    // Accessors - functions that get stuff
+    public String getName()            { return name; }
+    public int    getAge()             { return age; }
+
+}
