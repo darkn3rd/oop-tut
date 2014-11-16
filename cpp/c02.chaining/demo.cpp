8920dfd41a8c0f7607b004377cc156ec22ca9976
지훈 <sauvecali@yahoo.com>
2014-11-16 14:41:08 -0800

    C++ Constructor Chaining to Delegated Constructor

---
+++ cpp/c02.chaining/demo.cpp
@@ -0,0 +1,46 @@
+#include "Person.h"
+
+using namespace PersonSpace;
+
+int main(int argc, char* argv[])
+{
+    string name;
+    int    age;
+
+    // initialize data through overloaded constructor (set)
+    Person captain((char*)"Jean-Luc");  // instantiate /w name
+    Person officer(21);                 // instantiate /w age
+    Person ensign((char*)"Wesley", 15); // instantiate /w name and age
+
+    cout << endl;
+
+    // retrieve string from person object
+    name = captain.getName();
+    age  = captain.getAge();
+
+    // output results
+    cout << "Captain:" << endl;
+    cout << "\tName: " << name << endl;
+    cout << "\tAge: " << age << endl;
+
+    // retrieve string from person object
+    name = officer.getName();
+    age  = officer.getAge();
+
+    // output results
+    cout << "Officer:" << endl;
+    cout << "\tName: " << name << endl;
+    cout << "\tAge: " << age << endl;
+
+    // retrieve string from person object
+    name = ensign.getName();
+    age  = ensign.getAge();
+
+    // output results
+    cout << "Ensign:" << endl;
+    cout << "\tName: " << name << endl;
+    cout << "\tAge: " << age << endl;
+
+    // return exit of success
+    return 0;
+}
