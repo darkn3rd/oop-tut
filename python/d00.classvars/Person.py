8c5a8bbfebc6b73c3f737eb2a307ed7ae1c5f196
지훈 <sauvecali@yahoo.com>
2014-11-16 22:58:30 -0800

    Python - Class Variables

---
+++ python/d00.classvars/Person.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# class declaration
+class Person:
+  ###### Class Variables ######
+  count = 0
+
+  ###### Constructors ######
+  def __init__(self,name):
+    self.setName(name)    # call mutator
+    Person.count += 1     # incrememnt count
+    self.__info()
+
+  def __del__(self):
+    Person.count -= 1     # decrement count
+
+  ###### Methods ######
+  def __info(self):
+    print("[INFO] Person Object: '%s' created." % self.getName())
+
+  ###### Mutators ######
+  def setName(self,name):
+    self.name = name      # create/set instance variable
+
+  ###### Accessors ######
+  def getName(self):
+    return self.name      # return instance variable
+
+  @staticmethod
+  def getCount():
+    return Person.count
---
+++ python/d00.classvars/demo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python -B
+from Person import Person    # include Person.py
+
+print("\n\tThe number of Persons is now: '%s'.\n" % Person.getCount())
+
+print("Creating Persons...\n")
+
+# instantiate three Person objects
+captain = Person("Jean-Luc")
+officer = Person("Data")
+ensign  = Person("Wesley")
+
+print("\n\tThe number of Persons is now: '%s'.\n" % Person.getCount())
