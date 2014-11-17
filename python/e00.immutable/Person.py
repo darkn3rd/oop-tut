75b876e07f8d09e6774e16b2f45ab2947abedc80
지훈 <sauvecali@yahoo.com>
2014-11-16 23:36:40 -0800

    Python - Immutability (simulated)

---
+++ python/e00.immutable/Person.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# class declaration
+class Person:
+  ###### Initializers ######
+  def __init__(self,name, id_):
+    self.__id = id_    # indicate we want this private id variable
+    self.setName(name) # call mutator
+    self.__info()      # report info
+
+  ###### Methods ######
+  def __info(self):
+    print("[INFO] Person Object: '%s' created." % self.getName())
+    print("[INFO] \tCitizen Id No.: %s\n" % self.getId())
+
+  ###### Mutators ######
+  def setName (self,name):
+    self.name = name      # set data
+
+  ###### Accessors ######
+  def getName(self):
+    return self.name      # return data
+
+  def getId(self):
+    return self.__id      # return data
---
+++ python/e00.immutable/demo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python -B
+from Person import Person    # include Person.py
+
+# instantiate three Person objects on the heap
+captain = Person("Jean-Luc", 48382)
+officer = Person("Data", 38923)
+ensign  = Person("Wesley", 48234)
