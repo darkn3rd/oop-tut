d4844da03010af6ae74187d5933a52717f4e9e4a
Joaquín Menchaca <jmenchaca@verticalresponse.com>
2014-11-18 11:46:47 -0800

    Synchronize B0 - Java w/ Ruby

--- java/b00.construct/Demo.java
+++ java/b00.construct/Demo.java
@@ -1,13 +1,32 @@
 public class Demo {
     public static void main(String args[])
-    {      
-        // initialize data through constructor
-        Person person = new Person("Jean-Luc"); // instantiate with data
+    {   
+        String name;
 
-        // retrieve string from person object
-        String name = person.getName(); // accessor
+        System.out.println("Creating two objects:\n");
+
+        // create two objects
+        Person captain = new Person();       // instantiate new object
+        Person officer = new Person();       // instantiate new object
+        // initialize data through mutator (set)
+        captain.setName("Jean-Luc"); // call mutator
+        officer.setName("Data");     // call mutator
+
+        System.out.println("\nPrinting Results:\n");
+
+        // retrieve string from captain object through accessor (get)
+        name = captain.getName();   // call accessor
 
         // output results
-        System.out.println("Name of the Person:\n\t" + name);
+        System.out.println("  Name of the Captain:\n\t" + name);
+
+        // retrieve string from officer object through accessor (get)
+        name = officer.getName();   // call accessor
+
+        // output results
+        System.out.println("  Name of the Officer:\n\t" + name);
+
+        System.out.println("");
+
     }
 }
--- java/b00.construct/Person.java
+++ java/b00.construct/Person.java
@@ -3,11 +3,20 @@ public class Person
     private String name;
 
     // Constructor
-    public Person(String name)  { this.setName(name) }
+    public Person()  
+    { 
+        System.out.println("  [INFO]: Person Object is created");
+    }
     
     // Mutators - functions that set stuff	
-    public void   setName(String name) { this.name = name; }
+    public void   setName(String name) 
+    { 
+        this.name = name; 
+    }
 
     // Accessors - functions that get stuff
-    public String getName()            { return name; }
+    public String getName() 
+    { 
+        return name; 
+    }
 }
