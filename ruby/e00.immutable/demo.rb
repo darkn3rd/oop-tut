fe0b094c822613bade5003e2aa149a23cef922c9
Joaquín Menchaca <jmenchaca@verticalresponse.com>
2014-11-17 19:44:11 -0800

    Ruby - pretende immutability

---
+++ ruby/e00.immutable/demo.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/ruby
+require 'Person'                 # include Person.rb
+
+# instantiate three Person objects on the heap
+captain = Person.new "Jean-Luc", 48382
+officer = Person.new "Data", 38923
+ensign  = Person.new "Wesley", 48234
