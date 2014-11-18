61145aaa63011ebd1df6d6c1260e41f5f03cbaf2
Joaquín Menchaca <jmenchaca@verticalresponse.com>
2014-11-18 12:21:07 -0800

    Java - Sync B10 constructor to prior lesson

--- java/b10.construct/Demo.java
+++ java/b10.construct/Demo.java
@@ -1,13 +1,28 @@
 public class Demo {
     public static void main(String args[])
     {      
-        // initialize data through constructor
-        Person person = new Person("Jean-Luc"); // instantiate with data
+        String name;
 
-        // retrieve string from person object
-        String name = person.getName(); // accessor
+        System.out.println("Creating two objects:\n");
+
+        // create two objects
+        Person captain = new Person("Jean-Luc");       // instantiate new object
+        Person officer = new Person("Data");       // instantiate new object
+
+        System.out.println("\nPrinting Results:\n");
+
+        // retrieve string from captain object through accessor (get)
+        name = captain.getName();   // call accessor
 
         // output results
-        System.out.println("Name of the Person:\n\t" + name);
+        System.out.println("  Name of the Captain:\n\t" + name);
+
+        // retrieve string from officer object through accessor (get)
+        name = officer.getName();   // call accessor
+
+        // output results
+        System.out.println("  Name of the Officer:\n\t" + name);
+
+        System.out.println("");
     }
 }
--- java/b10.construct/Person.java
+++ java/b10.construct/Person.java
@@ -3,8 +3,15 @@ public class Person
     private String name;
 
     // Constructor
-    public Person(String name)  { this.setName(name) }
-    
+    public Person(String name)  
+    { 
+        System.out.println(
+            "  [INFO]: Person Object is created with name = \"" + 
+            name + "\"");
+        
+        this.setName(name);
+    } 
+
     // Mutators - functions that set stuff	
     public void   setName(String name) { this.name = name; }
 
