f0ea42d13fb761635e376d76d8359a6378a2caac
지훈 <sauvecali@yahoo.com>
2014-11-30 20:07:52 -0800

    Java Update

--- java/i00.abstract/Rectangle.java
+++ java/i00.abstract/Rectangle.java
@@ -1,7 +1,7 @@
 public class Rectangle extends Shape {
    /******* CONSTRUCTORS *******/
-   public Rectangle(Double length, Double width)  { super(length, width); }
-   public Rectangle(Integer length, Integer width)  { super(length, width); }
+   public Rectangle(Double length, Double width)   { super(length, width); }
+   public Rectangle(Integer length, Integer width) { super(length, width); }
 
    /******* METHODS *******/
    public Double calculateArea() {
--- java/j00.interface/Circle.java
+++
@@ -1,9 +0,0 @@
-public class Circle implements Shape {
-    private Double radius;
-
-    public Circle(Double radius)  { this.radius = radius; }
-
-    public double calculateArea() {
-      return (Math.pow(radius, 2) * Math.PI);
-    }
-}
