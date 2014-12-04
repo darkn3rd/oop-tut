e4b77dc7ee5cbe54684bfed1b4bd92bfe4010c86
Joaquín Menchaca <jmenchaca@verticalresponse.com>
2014-12-03 19:31:26 -0800

    Objective-C 2 - update declare

---
+++ objc-2/b10.declare/Demo.m
@@ -0,0 +1,24 @@
+#import <stdlib.h>
+#import "Person.h"
+
+// ###### Blocks ######
+void (^NSPrint)(NSString *) = ^(NSString *str) {
+    [str writeToFile:@"/dev/stdout" atomically:NO encoding:NSUTF8StringEncoding error:nil];
+};
+
+int main (int argc, const char * argv[])
+{
+    // istantiate new object
+    Person *captain = [[Person alloc] init]; // instantiate new object
+    // initialize data
+    [captain setName:@"Jean-Luc"];           // send message to set name
+
+    // retrieve string from person object
+    NSString *name = [captain name];         // send message to get name
+
+    // output results
+    NSPrint([NSString stringWithFormat: @"Name of the Person:\n\t%@\n", name]);
+
+    // return exit of success
+    return EXIT_SUCCESS;
+}
---
+++ objc-2/b10.declare/Person.h
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+#import <stdlib.h>
+
+// ###### Objects ######
+@interface Person : NSObject
+
+@property (assign) NSString *name;
+
+@end
