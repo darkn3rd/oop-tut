d323e8dd4af4ea538a0f02e6004250eb1c406e9f
지훈 <sauvecali@yahoo.com>
2014-12-24 07:16:35 -0800

    Objective-C 2: inheritance update

--- objective-c/objc-2/g10.parent/Student.m
+++ objective-c/objc-2/g10.parent/Student.m
@@ -4,10 +4,7 @@
 
 /******* Constructors *******/
 - (Student *) init: (NSString *) name gpa:(NSNumber*) gpa {
-    if (self = [super init]) {
-      self.name = name;
-      self.gpa = gpa;
-    }
+    if (self = [super init]) { self.name = name; self.gpa = gpa; }
 
     return self;
 }
