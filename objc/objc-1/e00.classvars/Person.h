08fc23b4c1f385bda52e09a4474d6447f31dd574
지훈 <sauvecali@yahoo.com>
2014-12-23 20:50:23 -0800

    Objective-C: Class Variable update to supporet properties

--- objc/objc-1/e00.classvars/Person.h
+++ objc/objc-1/e00.classvars/Person.h
@@ -3,22 +3,18 @@
 #import <stdio.h>
 #import <string.h>
 
-@interface Person : Object
-{
+@interface Person : Object {
   // ###### Instance Variables ######
-  char* name;
+  char* _name;
 }
+
+@property const char *name;
 
 // ###### Constructors ######
 - (Person *) init;
 - (Person *) initWithName: (const char *) n;
-// ###### Destructors ######
-- (void) dealloc;
 // ###### Methods ######
 - (void) info;
-// ###### Mutators ######
-- (void) setName: (const char *) n;
 // ###### Accessors ######
-- (const char *) getName;
 + (int) getCount;
 @end
--- objc/objc-1/e00.classvars/Person.m
+++ objc/objc-1/e00.classvars/Person.m
@@ -13,34 +13,19 @@ static int count = 0;
 
 - (Person *) initWithName: (const char *) n {
     self = [super init];
-    if (self) { [self setName: n]; ++count; [self info]; }
+    if (self) { self.name = n; ++count; [self info]; }
     return self;
-}
-
-// ###### Destructors ######
-- (void) dealloc {
-    --count;
-#if !__has_feature(objc_arc)
-    [super dealloc];
-#endif
 }
 
 // ###### Methods ######
 -  (void) info {
-    printf("[INFO] Person Object: name='%s' created.\n", [self getName]);
+    printf("[INFO] Person Object: name='%s' created.\n", self.name);
 }
 
-// ###### Mutators ######
-- (void) setName: (const char *) n {
-    if (n) {
-       // create new char* of length of n
-       name = (char *) malloc(strlen(n)+1);
-       strcpy(name, n); // copy contents
-    }
-}
+// ###### Properties ######
+@synthesize name = _name;
 
 // ###### Accessors ######
-- (const char *) getName { return name; }
 + (int) getCount { return count; }
 
 
